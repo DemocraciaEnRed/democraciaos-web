8b19ba96dab257e3745ba3913e4efd0a9502126e
Josh Chisnerman <stranyer@gmail.com>
2020-10-02 10:41:33 -0300

    added typeform and updates

--- src/components/instalations-section/index.js
+++ src/components/instalations-section/index.js
@@ -1,6 +1,7 @@
 import React from "react"
 import "./styles.scss"
 import { useIntl } from "gatsby-plugin-intl"
+import { ReactTypeformEmbed } from 'react-typeform-embed';
 
 import diy from "./assets/diy.svg"
 import standard from "./assets/standard.svg"
@@ -41,8 +42,12 @@ export default ({ data }) =>  {
                             </div>
                         )}
                     </div>
+                
+
+                    <div className="ExampleWidget">
+                    <ReactTypeformEmbed popup={false} url="https://form.typeform.com/to/bkXtFW" />
+                </div>
             </div>
-            <div class="typeform-widget" data-url="https://form.typeform.com/to/bkXtFW" data-transparency="50" data-hide-headers=true data-hide-footer=true style="width: 100%; height: 500px;"></div> <script> (function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() </script> <div style="font-family: Sans-Serif;font-size: 12px;color: #999;opacity: 0.5; padding-top: 5px;"> powered by <a href="https://admin.typeform.com/signup?utm_campaign=bkXtFW&utm_source=typeform.com-01D8JT2MK6CEEY9P7R38Q3Q9RT-professional&utm_medium=typeform&utm_content=typeform-embedded-poweredbytypeform&utm_term=ES" style="color: #999" target="_blank">Typeform</a> </div>
             </section>
 
             
